0280d7ab51e1429b840b30bc320c5d0c443a8237
AbhishekFulmali <113376607+AbhishekFulmali@users.noreply.github.com>
2022-09-13 16:47:21 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+import java.util.Scanner;
+
+class even
+{
+   public static void main (String args[])
+   {
+          int num=23;
+          if(num%2==0)
+		  {
+			  
+			 System.out.println("Enter number is even");
+		  }	
+          else
+		  {
+			  
+			  System.out.println("Enter number is odd");
+		  }			  
+   }
+}
